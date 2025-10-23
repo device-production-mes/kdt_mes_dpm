3cae49e44c1010a9c078349603faaa2c93408f6e
admin <ug034516@gmail.com>
2025-10-23 14:45:36 +0900

    feat: controller, dao

--- src/main/java/com/device_production_mes/team5/Console.java
+++ src/main/java/com/device_production_mes/team5/Console.java
@@ -1,10 +1,17 @@
 package com.device_production_mes.team5;
 
+import com.device_production_mes.team5.controller.MainController;
+import lombok.RequiredArgsConstructor;
 import org.springframework.boot.CommandLineRunner;
+import org.springframework.stereotype.Component;
 
+@Component
+@RequiredArgsConstructor
 public class Console implements CommandLineRunner {
+    private final MainController mainController;
+
     @Override
     public void run(String... args) throws Exception {
-
+        mainController.menu();
     }
 }
